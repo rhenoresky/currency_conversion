e9158175e89348e3acf6771ed773a1e46cce6fe9
Rheno Resky Nugraha <rheno2002@gmail.com>
2024-11-28 23:38:00 +0700

    add particle

--- src/app/projects/currency-conversion/currency-conversion.component.ts
+++ src/app/projects/currency-conversion/currency-conversion.component.ts
@@ -6,10 +6,13 @@ import { LovComponent } from '@core/shared-component/lov/lov.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CurrencyModel, ResultConversionModel } from './model/currency.model';
 
+declare let particlesJS: any;
+
 @Component({
     selector: 'app-currency-conversion',
     templateUrl: './currency-conversion.component.html',
     styleUrls: ['./currency-conversion.component.scss'],
+    animations: [],
 })
 export class CurrencyConversionComponent {
     @ViewChild('TableComponent') table: TableComponent;
@@ -37,6 +40,62 @@ export class CurrencyConversionComponent {
     ngOnInit(): void {
         this.getLocalStorageCurrency();
         this.createFormConversion();
+        this.invokeParticles();
+    }
+
+    invokeParticles(): void {
+        particlesJS('particles-js', {
+            particles: {
+                number: {
+                    value: 160,
+                    density: { enable: true, value_area: 800 },
+                },
+                color: { value: '#ffffff' },
+                shape: {
+                    type: 'circle',
+                    stroke: { width: 0, color: '#000000' },
+                    polygon: { nb_sides: 5 },
+                    image: { src: 'img/github.svg', width: 100, height: 100 },
+                },
+                opacity: {
+                    value: 1,
+                    random: true,
+                    anim: {
+                        enable: true,
+                        speed: 1,
+                        opacity_min: 0,
+                        sync: false,
+                    },
+                },
+                size: {
+                    value: 3,
+                    random: true,
+                    anim: {
+                        enable: false,
+                        speed: 4,
+                        size_min: 0.3,
+                        sync: false,
+                    },
+                },
+                line_linked: {
+                    enable: false,
+                    distance: 150,
+                    color: '#ffffff',
+                    opacity: 0.4,
+                    width: 1,
+                },
+                move: {
+                    enable: true,
+                    speed: 1,
+                    direction: 'none',
+                    random: true,
+                    straight: false,
+                    out_mode: 'out',
+                    bounce: false,
+                    attract: { enable: false, rotateX: 600, rotateY: 600 },
+                },
+            },
+        });
     }
 
     createFormConversion(): void {
